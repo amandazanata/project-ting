16a12ad7079e0455901a6d31fed892775582e542
Amanda Zanata <amandazanata46@gmail.com>
2023-08-03 22:52:12 -0300

    Entrega Projeto

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    return aux_search(word, instance, include_content=False)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    return aux_search(word, instance)
+
+
+def aux_search(word, instance, include_content=True):
+    def create_occurrence(index, line):
+        occurrence = {"linha": index}
+        if include_content:
+            occurrence["conteudo"] = line
+        return occurrence
+
+    def has_word(line):
+        return word.casefold() in line.casefold()
+
+    new_instance = [
+        {
+            "palavra": word,
+            "arquivo": dictionary["nome_do_arquivo"],
+            "ocorrencias": [
+                create_occurrence(index + 1, line)
+                for index, line in enumerate(dictionary["linhas_do_arquivo"])
+                if has_word(line)
+            ]
+        }
+        for dictionary in instance.data
+        if any(has_word(line) for line in dictionary["linhas_do_arquivo"])
+    ]
+
+    return new_instance
